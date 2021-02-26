8f2ea47b1fd5dc154ade973058beb2a3e43f28ea
unknown <you@example.com>
2021-02-26 15:47:34 +0530

    2 core java programs added

---
+++ Core_java_Practice/stest.java
@@ -0,0 +1,13 @@
+class stest
+{
+	public void display()
+	{
+		System.out.println("hello from display");
+	}
+	public static void main(String[] arg)
+	{
+		stest s=new stest();
+		s.display();
+
+	}
+}
