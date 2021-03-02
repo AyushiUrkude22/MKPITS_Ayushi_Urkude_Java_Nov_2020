68e83a0b0d83e91c7ac5015bf00964562b85f760
unknown <you@example.com>
2021-03-02 11:06:24 +0530

    all folder to be commited

--- Core_java_Practice/WelcomeEx.java
+++
@@ -1,9 +0,0 @@
-//wap to declare a variable num and store value 5 in it and then print it.
-class WelcomeEx
-{
-	public static void main(String[] arg)
-	{
-		int num=5;
-		System.out.println("number = " + num);
-	}
-}
