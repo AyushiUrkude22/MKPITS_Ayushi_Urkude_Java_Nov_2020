74c97c6d903830c8e0791f093d85d57a348db516
unknown <you@example.com>
2021-03-06 11:40:40 +0530

    all folder to be commited

---
+++ Core_Java_Practice/book.java
@@ -0,0 +1,16 @@
+//5) Java program to create a class book having fields bookname,author and price
+//and a blank constructor and constructor with 3 parameters.
+
+class book
+{
+	String bookname;
+	String author;
+	float price;
+	//creating a blank constructor
+	public  book()
+	{
+		bookname="Java Programming";
+		author="Mayuri";
+		price=250.00f;
+
+	}
---
+++ Core_Java_Practice/book4main.java
@@ -0,0 +1,29 @@
+// Java program to create a class book having fields bookname,author and price and a blank constructor.
+
+class book
+{
+	String bookname;
+	String author;
+	float price;
+	//creating a blank constructor
+	public  book()
+	{
+		bookname="Java Programming";
+		author="Ayushi";
+		price=280.0f;
+	}
+	public void showdata()
+	{
+		System.out.println("Bookname = " + bookname);
+		System.out.println("Author = " + author);
+		System.out.println("Price = " + price);
+	}
+}
+class book4main
+{
+	public static void main(String[] args)
+	{
+		book b=new book(); // automatically it will call blank constructor
+		b.showdata();
+	}
+}
