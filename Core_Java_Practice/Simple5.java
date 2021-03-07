0f8373a7e96962f8446513ef5363b260d3e19ab5
unknown <you@example.com>
2021-03-07 13:36:08 +0530

    all folder to be commited

---
+++ Core_Java_Practice/Simple5.java
@@ -0,0 +1,20 @@
+//Java Exception Keywords
+
+public class Simple5{
+public static void main(String args[]){
+
+int n1=10;
+int n2=0;
+int res=0;
+try
+{
+res=n1/n2;
+}catch(Exception ee)
+{
+System.out.println(ee.toString());
+}
+System.out.println("res = " + res);
+
+}
+
+}
